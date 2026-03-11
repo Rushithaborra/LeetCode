c75502521dad9923b74fc741cbd72cfaa8b5f504
Rushithaborra <rushithaborra@gmail.com>
2026-03-11 17:37:15 +0530

    Time: 46 ms (74.30%) | Memory: 13.1 MB (57.89%) - LeetSync

---
+++ 654-maximum-binary-tree/maximum-binary-tree.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def constructMaximumBinaryTree(self, nums):
+        if not nums:
+            return None
+        
+        max_val = max(nums)
+        index = nums.index(max_val)
+        
+        root = TreeNode(max_val)
+        root.left = self.constructMaximumBinaryTree(nums[:index])
+        root.right = self.constructMaximumBinaryTree(nums[index+1:])
+        
+        return root
