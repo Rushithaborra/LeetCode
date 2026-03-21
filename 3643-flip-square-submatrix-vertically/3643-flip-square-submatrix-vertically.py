b521c4b993894016acb12158d1dd12f364c4e3ee
Rushithaborra <rushithaborra@gmail.com>
2026-03-21 06:42:36 -0500

    Time: 2 ms (40.74%), Space: 12.5 MB (100%) - LeetHub

---
+++ 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def reverseSubmatrix(self, grid, x, y, k):
+        for i in range(k // 2):
+            for j in range(y, y + k):
+                # Swap elements vertically
+                grid[x + i][j], grid[x + k - 1 - i][j] = grid[x + k - 1 - i][j], grid[x + i][j]
+        
+        return grid
