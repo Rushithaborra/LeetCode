7d730e203a0ef9242964507d5641274e0cff17b9
Rushithaborra <rushithaborra@gmail.com>
2026-03-24 06:48:36 -0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (45.07%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        words = s.split()
+        
+        if len(pattern) != len(words):
+            return False
+        
+        p_to_w = {}
+        w_to_p = {}
+        
+        for p, w in zip(pattern, words):
+            
+            # Check pattern → word
+            if p in p_to_w:
+                if p_to_w[p] != w:
+                    return False
+            else:
+                p_to_w[p] = w
+            
+            # Check word → pattern
+            if w in w_to_p:
+                if w_to_p[w] != p:
+                    return False
+            else:
+                w_to_p[w] = p
+        
+        return True
