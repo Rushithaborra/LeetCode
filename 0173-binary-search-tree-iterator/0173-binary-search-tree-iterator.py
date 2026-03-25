0db5465bcc66015956129ac70a565658b243ed21
Rushithaborra <rushithaborra@gmail.com>
2026-03-25 08:59:39 -0500

    Time: 16 ms (49.36%), Space: 21.1 MB (63.12%) - LeetHub

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -0,0 +1,19 @@
+class BSTIterator(object):
+
+    def __init__(self, root):
+        self.stack = []
+        self._pushLeft(root)
+
+    def _pushLeft(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
+
+    def next(self):
+        node = self.stack.pop()
+        if node.right:
+            self._pushLeft(node.right)
+        return node.val
+
+    def hasNext(self):
+        return len(self.stack) > 0
