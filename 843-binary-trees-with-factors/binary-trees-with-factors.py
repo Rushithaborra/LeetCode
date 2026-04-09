ac4fc83a27f4f0b63c3bacde7c31f1b7895751d7
Rushithaborra <rushithaborra@gmail.com>
2026-04-09 20:32:39 +0530

    Time: 224 ms (73.33%) | Memory: 12.5 MB (66.67%) - LeetSync

---
+++ 843-binary-trees-with-factors/binary-trees-with-factors.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def numFactoredBinaryTrees(self, arr):
+        MOD = 10**9 + 7
+        arr.sort()
+        
+        dp = {}
+        
+        for i in range(len(arr)):
+            x = arr[i]
+            dp[x] = 1 
+            
+            for j in range(i):
+                a = arr[j]
+                if x % a == 0:
+                    b = x // a
+                    if b in dp:
+                        dp[x] += dp[a] * dp[b]
+                        dp[x] %= MOD
+        
+        return sum(dp.values()) % MOD
