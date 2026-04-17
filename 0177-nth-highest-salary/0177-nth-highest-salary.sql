cb561f33f241b11fb63304229db74bfba0bc682e
Rushithaborra <rushithaborra@gmail.com>
2026-04-17 19:55:48 +0530

    Time: 441 ms (41.15%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE m INT;
+SET m = N - 1;
+  RETURN (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET m
+  );
+END;
