1000bae4c5d0a879d99ad14affd334898d9149d1
Rushithaborra <rushithaborra@gmail.com>
2026-05-15 16:13:51 +0530

    Time: 3 ms (73.76%), Space: 12.1 MB (99.66%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        freq = {}
+        length = 0
+        odd_found = False
+        for ch in s:
+            freq[ch] = freq.get(ch, 0) + 1
+        for count in freq.values():
+            if count % 2 == 0:
+                length += count
+            else:
+                length += count - 1
+                odd_found = True
+        if odd_found:
+            length += 1
+
+        return length
