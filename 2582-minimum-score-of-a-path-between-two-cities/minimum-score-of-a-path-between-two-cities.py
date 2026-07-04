5c2a5563fec80d3a930b098156c99254b9126e5c
Rushithaborra <rushithaborra@gmail.com>
2026-07-04 22:38:18 +0530

    Time: 389 ms (28.30%) | Memory: 72 MB (62.26%) - LeetSync

---
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,27 @@
+from collections import defaultdict, deque
+
+class Solution(object):
+    def minScore(self, n, roads):
+        graph = defaultdict(list)
+
+        for u, v, d in roads:
+            graph[u].append((v, d))
+            graph[v].append((u, d))
+
+        visited = set()
+        q = deque([1])
+        visited.add(1)
+
+        ans = float('inf')
+
+        while q:
+            node = q.popleft()
+
+            for nei, dist in graph[node]:
+                ans = min(ans, dist)
+
+                if nei not in visited:
+                    visited.add(nei)
+                    q.append(nei)
+
+        return ans
