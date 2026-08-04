50b3bae07332efb2dda037d99dd0ea3c67ff7b88
Rushithaborra <rushithaborra@gmail.com>
2026-08-04 17:17:15 +0530

    Time: 7 ms (52.53%) | Memory: 12.3 MB (87.79%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findMissingElements(self, nums):
+        s = set(nums)
+        ans = []
+        for x in range(min(nums), max(nums) + 1):
+            if x not in s:
+                ans.append(x)
+        return ans
