94552f3f4d8c0f0344d94950fcfafecb1473ae83
Rushithaborra <rushithaborra@gmail.com>
2026-09-13 23:32:19 +0530

    Time: 1911 ms (16.42%) | Memory: 12.5 MB (80.60%) - LeetSync

---
+++ 864-image-overlap/image-overlap.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def largestOverlap(self, img1, img2):
+        n = len(img1)
+        ans = 0
+
+        for dx in range(-n + 1, n):
+            for dy in range(-n + 1, n):
+                count = 0
+
+                for i in range(n):
+                    for j in range(n):
+                        x = i + dx
+                        y = j + dy
+
+                        if 0 <= x < n and 0 <= y < n:
+                            if img1[i][j] == 1 and img2[x][y] == 1:
+                                count += 1
+
+                ans = max(ans, count)
+
+        return ans
